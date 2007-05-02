1da6dc7ecf4ca3cf8d68f50128edd998d93c7ba1
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2007-05-02 11:39:49 +0000

    Small fix. <_<

--- trunk/iDB/inc/replys.php
+++ trunk/iDB/inc/replys.php
@@ -11,7 +11,7 @@
     Copyright 2004-2007 Cool Dude 2k - http://idb.berlios.de/
     Copyright 2004-2007 Game Maker 2k - http://upload.idb.s1.jcink.com/
 
-    $FileInfo: replys.php - Last Update: 04/27/2007 SVN 49 - Author: cooldude2k $
+    $FileInfo: replys.php - Last Update: 05/02/2007 SVN 52 - Author: cooldude2k $
 */
 $File1Name = dirname($_SERVER['SCRIPT_NAME'])."/";
 $File2Name = $_SERVER['SCRIPT_NAME'];

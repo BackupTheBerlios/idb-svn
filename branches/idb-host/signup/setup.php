6eb78cd3b14472bffbd615ea6e77126424f8f6ac
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2007-10-16 23:23:15 +0000

    Small fix to HTML style. :)

--- branches/idb-host/signup/setup.php
+++ branches/idb-host/signup/setup.php
@@ -12,7 +12,7 @@
     Copyright 2004-2007 Game Maker 2k - http://upload.idb.s1.jcink.com/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: setup.php - Last Update: 07/01/2007 SVN 35 - Author: cooldude2k $
+    $FileInfo: setup.php - Last Update: 10/16/2007 SVN 118 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="setup.php"||$File3Name=="/setup.php") {
@@ -31,7 +31,7 @@ if(!$StatSQL) { $Error="Yes";
 echo "<span class=\"TableMessage\">";
 echo "<br />".mysql_errno().": ".mysql_error()."\n</span>\n"; }
 ?>
-<form method="post" name="install" id="install" action="signup.php?act=Part3">
+<form style="display: inline;" method="post" name="install" id="install" action="signup.php?act=Part3">
 <table style="text-align: left;">
 <tr style="text-align: left;">
 	<td style="width: 50%;"><label class="TextBoxLabel" for="NewBoardName">Insert Board Name:</label></td>

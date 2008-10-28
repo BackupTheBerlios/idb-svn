1a4cdd182c783ee27eee1b6d8399424f488ab88b
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2008-10-28 06:23:47 +0000

    Added IPB Alt Theme by eternalSHADOW.com

---
+++ branches/themes/IPBAlt/info.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    $ThemeInfo - Name: IPB Alt Theme - Author: eternalSHADOW.com $
+*/
+$ThemeInfo = array();
+$ThemeInfo['ThemeName'] = "IPB Alt Theme";
+$ThemeInfo['ThemeMaker'] = "eternalSHADOW.com";
+$ThemeInfo['ThemeVersion'] = "0.2.1";
+$ThemeInfo['ThemeVersionType'] = "Pre-Alpha";
+$ThemeInfo['ThemeSubVersion'] = "SVN 144";
+$ThemeInfo['MakerURL'] = "http://eternalshadow.com/";
+$ThemeInfo['CopyRight'] = $ThemeInfo['ThemeName']." was made by <a href=\"".$ThemeInfo['MakerURL']."\" title=\"".$ThemeInfo['ThemeMaker']."\">".$ThemeInfo['ThemeMaker']."</a>";
+?>
---
+++ branches/themes/IPBAlt/settings.php
@@ -0,0 +1,52 @@
+<?php
+/*
+    $ThemeInfo - Name: IPB Alt Theme - Author: eternalSHADOW.com $
+*/
+$ThemeSet = array();
+$ThemeSet['ThemeName'] = "IPB Alt Theme";
+$ThemeSet['ThemeMaker'] = "eternalSHADOW.com";
+$ThemeSet['ThemeVersion'] = "0.2.1";
+$ThemeSet['ThemeVersionType'] = "Pre-Alpha";
+$ThemeSet['ThemeSubVersion'] = "SVN 144";
+$ThemeSet['MakerURL'] = "http://eternalshadow.com/";
+$ThemeSet['CopyRight'] = $ThemeSet['ThemeName']." was made by <a href=\"".$ThemeSet['MakerURL']."\" title=\"".$ThemeSet['ThemeMaker']."\">".$ThemeSet['ThemeMaker']."</a>";
+$ThemeSet['CSS'] = "themes/IPBAlt/css.css";
+$ThemeSet['CSSType'] = "include";
+$ThemeSet['FavIcon'] = "themes/IPBAlt/favicon.ico";
+$ThemeSet['PreLogo'] = "<div style=\"text-align: center;\">";
+$ThemeSet['Logo'] = $Settings['board_name'];
+$ThemeSet['LogoStyle'] = "font-size: 40px; font-family: verdana, arial, sans-serif; color: black; text-decoration: none;";
+$ThemeSet['SubLogo'] = "</div>";
+$ThemeSet['TopicIcon'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/topic.gif\" alt=\"Topic\" title=\"Topic\" /></div>";
+$ThemeSet['HotTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/hot.gif\" alt=\"Hot Topic\" title=\"Hot Topic\" /></div>";
+$ThemeSet['PinTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/pin.gif\" alt=\"Pinned!\" title=\"Pinned Topic!\" /></div>";
+$ThemeSet['HotPinTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/pin.gif\" alt=\"Pinned!\" title=\"Pinned Topic!\" /></div>";
+$ThemeSet['ClosedTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/lock.gif\" alt=\"Closed!\" title=\"Closed Topic!\" /></div>";
+$ThemeSet['HotClosedTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/lock.gif\" alt=\"Closed!\" title=\"Closed Topic!\" /></div>";
+$ThemeSet['PinClosedTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/lock.gif\" alt=\"Closed!\" title=\"Pinned Closed Topic!\" /></div>";
+$ThemeSet['HotPinClosedTopic'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/lock.gif\" alt=\"Closed!\" title=\"Pinned Closed Topic!\" /></div>";
+$ThemeSet['MessageRead'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/read.gif\" alt=\"Message!\" title=\"Message!\" /></div>";
+$ThemeSet['MessageUnread'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/unread.gif\" alt=\"New Message!\" title=\"New Message!\" /></div>";
+$ThemeSet['Profile'] = "<img src=\"themes/IPBAlt/profile.gif\" alt=\"Profile\" title=\"Profile\" />";
+$ThemeSet['WWW'] = "<img src=\"themes/IPBAlt/www.gif\" alt=\"WWW\" title=\"WWW\" />";
+$ThemeSet['PM'] = "<img src=\"themes/IPBAlt/pm.gif\" alt=\"PM\" title=\"PM\" />";
+$ThemeSet['TopicLayout'] = "Type 1";
+$ThemeSet['AddReply'] = "<img src=\"themes/IPBAlt/reply.gif\" alt=\"Add Reply\" title=\"Add Reply\" />";
+$ThemeSet['FastReply'] = "<img src=\"themes/IPBAlt/fast.gif\" alt=\"Fast Reply\" title=\"Fast Reply\" />";
+$ThemeSet['NewTopic'] = "<img src=\"themes/IPBAlt/post.gif\" alt=\"New Topic\" title=\"New Topic\" />";
+$ThemeSet['QuoteReply'] = "<img src=\"themes/IPBAlt/quote.gif\" alt=\"Quote Reply\" title=\"Quote Reply\" />";
+$ThemeSet['Report'] = "<img src=\"themes/IPBAlt/report.gif\" alt=\"Report Reply\" title=\"Report Reply\" />";
+$ThemeSet['EditReply'] = "<img src=\"themes/IPBAlt/edit.gif\" alt=\"Edit Reply\" title=\"Edit Reply\" />";
+$ThemeSet['DeleteReply'] = "<img src=\"themes/IPBAlt/delete.gif\" alt=\"Delete Reply\" title=\"Delete Reply\" />";
+$ThemeSet['LineDivider'] = "&nbsp;|&nbsp;";
+$ThemeSet['ButtonDivider'] = "&nbsp;";
+$ThemeSet['LineDividerTopic'] = "&nbsp;";
+$ThemeSet['TitleDivider'] = "-&gt;";
+$ThemeSet['ForumIcon'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/forum.gif\" alt=\"Forum\" title=\"Forum\" /></div>";
+$ThemeSet['SubForumIcon'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/subforum.gif\" alt=\"SubForum\" title=\"SubForum\" /></div>";
+$ThemeSet['RedirectIcon'] = "<div style=\"text-align: center;\"><img src=\"themes/IPBAlt/redirect.gif\" alt=\"Redirect Forum\" title=\"Redirect Forum\" /></div>";
+$ThemeSet['TitleIcon'] = "<img src=\"themes/IPBAlt/nav.gif\" alt=\"-&gt;\" title=\"-&gt;\" />&nbsp;";
+$ThemeSet['StatsIcon'] = "<img src=\"themes/IPBAlt/stats.gif\" alt=\"Board Stats\" title=\"Board Stats\" />";
+$ThemeSet['NoAvatar'] = "themes/IPBAlt/noavatar.png";
+$ThemeSet['NoAvatarSize'] = "100x100";
+?>

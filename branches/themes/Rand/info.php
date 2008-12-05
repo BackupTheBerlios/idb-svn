b11106aa11db6a4e2341f290fa9504fdd7f1cae9
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2008-12-05 22:39:59 +0000

    Added new Radnom iDB Theme.

---
+++ branches/themes/Rand/info.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2004-2008 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2004-2008 Game Maker 2k - http://intdb.sourceforge.net/
+    $ThemeInfo - Name: iDB Theme - Author: cooldude2k $
+    $FileInfo: info.php - Last Update: 12/01/2008 SVN 194 - Author: cooldude2k $
+*/
+$ThemeInfo = array();
+$ThemeInfo['ThemeName'] = "Random iDB Theme";
+$ThemeInfo['ThemeMaker'] = "Kazuki";
+$ThemeInfo['ThemeVersion'] = "0.2.3";
+$ThemeInfo['ThemeVersionType'] = "Pre-Alpha";
+$ThemeInfo['ThemeSubVersion'] = "SVN 194";
+$ThemeInfo['MakerURL'] = "http://kazuki.homelinux.org/bbs/category.php?act=view&amp;id=2";
+$ThemeInfo['CopyRight'] = $ThemeInfo['ThemeName']." was made by <a href=\"".$ThemeInfo['MakerURL']."\" title=\"".$ThemeInfo['ThemeMaker']."\">".$ThemeInfo['ThemeMaker']."</a>";
+?>
---
+++ branches/themes/Rand/settings.php
@@ -0,0 +1,66 @@
+<?php
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2004-2008 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2004-2008 Game Maker 2k - http://intdb.sourceforge.net/
+    $ThemeInfo - Name: Random iDB Theme - Author: cooldude2k $
+    $FileInfo: settings.php - Last Update: 12/01/2008 SVN 194 - Author: cooldude2k $
+*/
+$ThemeSet = array();
+$ThemeSet['ThemeName'] = "Random iDB Theme";
+$ThemeSet['ThemeMaker'] = "Kazuki";
+$ThemeSet['ThemeVersion'] = "0.2.3";
+$ThemeSet['ThemeVersionType'] = "Pre-Alpha";
+$ThemeSet['ThemeSubVersion'] = "SVN 194";
+$ThemeSet['MakerURL'] = "http://kazuki.homelinux.org/bbs/category.php?act=view&amp;id=2";
+$ThemeSet['CopyRight'] = $ThemeSet['ThemeName']." was made by <a href=\"".$ThemeSet['MakerURL']."\" title=\"".$ThemeSet['ThemeMaker']."\">".$ThemeSet['ThemeMaker']."</a>";
+$ThemeSet['CSS'] = "themes/Rand/css.css";
+$ThemeSet['CSSType'] = "include";
+$ThemeSet['FavIcon'] = "themes/Rand/favicon.ico";
+$ThemeSet['TableStyle'] = "div";
+$ThemeSet['PreLogo'] = null;
+$ThemeSet['Logo'] = $Settings['board_name'];
+$ThemeSet['LogoStyle'] = "text-decoration: none;";
+$ThemeSet['SubLogo'] = null;
+$ThemeSet['TopicIcon'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"Topic!\">&nbsp;(T)&nbsp;</div>";
+$ThemeSet['HotTopic'] = "<div style=\"text-align: center; font-size: 11px; font-weight: bold;\" title=\"Hot Topic!\">&nbsp;(T)&nbsp;</div>";
+$ThemeSet['PinTopic'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"Pinned Topic!\">&nbsp;{P}&nbsp;</div>";
+$ThemeSet['HotPinTopic'] = "<div style=\"text-align: center; font-size: 11px; font-weight: bold;\" title=\"Hot Pinned Topic!\">&nbsp;{P}&nbsp;</div>";
+$ThemeSet['ClosedTopic'] = "<div style=\"text-align: center; font-size: 11px; text-decoration: line-through;\" title=\"Closed Topic!\">&nbsp;[T]&nbsp;</div>";
+$ThemeSet['HotClosedTopic'] = "<div style=\"text-align: center; font-size: 11px; text-decoration: line-through; font-weight: bold;\" title=\"Hot Closed Topic!\">&nbsp;[T]&nbsp;</div>";
+$ThemeSet['PinClosedTopic'] = "<div style=\"text-align: center; font-size: 11px; text-decoration: line-through;\" title=\"Closed Pinned Topic!\">&nbsp;[P]&nbsp;</div>";
+$ThemeSet['HotPinClosedTopic'] = "<div style=\"text-align: center; font-size: 11px; text-decoration: line-through; font-weight: bold;\" title=\"Hot Closed Pinned Topic!\">&nbsp;[P]&nbsp;</div>";
+$ThemeSet['MessageRead'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"Message!\">&nbsp;[M]&nbsp;</div>";
+$ThemeSet['MessageUnread'] = "<div style=\"text-align: center; font-size: 11px; font-weight: bold;\" title=\"New Message!\">&nbsp;(M)&nbsp;</div>";
+$ThemeSet['Profile'] = "Profile";
+$ThemeSet['WWW'] = "WWW";
+$ThemeSet['PM'] = "PM";
+$ThemeSet['TopicLayout'] = "Type 1";
+$ThemeSet['AddReply'] = "<span style=\"color: white; font-size: 25px;\" title=\"Add Reply\">Add Reply</span>";
+$ThemeSet['FastReply'] = "<span style=\"color: white; font-size: 25px;\" title=\"Fast Reply\">Fast Reply</span>";
+$ThemeSet['NewTopic'] = "<span style=\"color: white; font-size: 25px;\" title=\"New Topic\">New Topic</span>";
+$ThemeSet['QuoteReply'] = "Quote";
+$ThemeSet['EditReply'] = "Edit";
+$ThemeSet['DeleteReply'] = "Delete";
+$ThemeSet['Report'] = "Report";
+$ThemeSet['LineDivider'] = "&nbsp;|&nbsp;";
+$ThemeSet['ButtonDivider'] = "&nbsp;&nbsp;&nbsp;";
+$ThemeSet['LineDividerTopic'] = "&nbsp;|&nbsp;";
+$ThemeSet['TitleDivider'] = "-&gt;";
+$ThemeSet['ForumIcon'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"Forum\">&nbsp;(F)&nbsp;</div>";
+$ThemeSet['SubForumIcon'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"SubForum\">&nbsp;{SF}&nbsp;</div>";
+$ThemeSet['RedirectIcon'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"Redirect Forum\">&nbsp;[RF]&nbsp;</div>";
+$ThemeSet['TitleIcon'] = null;
+$ThemeSet['NavLinkIcon'] = null;
+$ThemeSet['NavLinkDivider'] = "&nbsp;-&gt;&nbsp;";
+$ThemeSet['StatsIcon'] = "<div style=\"text-align: center; font-size: 11px;\" title=\"Board Stats\"><br />(S)<br /></div>&nbsp;";
+$ThemeSet['NoAvatar'] = "themes/iDB/noavatar.png";
+$ThemeSet['NoAvatarSize'] = "100x100";
+?>

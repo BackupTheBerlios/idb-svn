13e577f3fc9238bbb86361edd7b62327b386aebb
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-11-15 05:04:51 +0000

    Another update to SQL Commands. :P

--- branches/idb-host/inc/admin/main.php
+++ branches/idb-host/inc/admin/main.php
@@ -11,7 +11,7 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: main.php - Last Update: 10/08/2009 SVN 324 - Author: cooldude2k $
+    $FileInfo: main.php - Last Update: 11/14/2009 SVN 347 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="main.php"||$File3Name=="/main.php") {
@@ -83,7 +83,7 @@ if($_GET['act']=="delsessions"&&$GroupInfo['ViewDBInfo']=="yes") {
 $time = GMTimeStamp() - ini_get("session.gc_maxlifetime");
 //$sqlg = query('DELETE FROM `'.$Settings['sqltable'].'sessions` WHERE `expires` < UNIX_TIMESTAMP();', array(null));
 $sqlgc = query('DELETE FROM `'.$Settings['sqltable'].'sessions` WHERE `expires` < %i', array($time));
-mysql_query($sqlgc);
+exec_query($sqlgc);
 $_POST['update'] = "now"; $_GET['act'] = "view"; }
 ?>
 </td>
@@ -178,7 +178,7 @@ $_SESSION = array();
 @session_unset();
 @session_destroy();
 $delboard = 'DROP TABLE `'.$_GET['board'].'_categories`, `'.$_GET['board'].'_catpermissions`, `'.$_GET['board'].'_events`, `'.$_GET['board'].'_forums`, `'.$_GET['board'].'_groups`, `'.$_GET['board'].'_members`, `'.$_GET['board'].'_messenger`, `'.$_GET['board'].'_permissions`, `'.$_GET['board'].'_posts`, `'.$_GET['board'].'_restrictedwords`, `'.$_GET['board'].'_sessions`, `'.$_GET['board'].'_smileys`, `'.$_GET['board'].'_topics`, `'.$_GET['board'].'_wordfilter`';
-mysql_query($delboard);
+exec_query($delboard);
 @redirect("refresh",$basedir.url_maker($exfile['index'],$Settings['file_ext'],"act=view",$Settings['qstr'],$Settings['qsep'],$prexqstr['index'],$exqstr['index'],FALSE),"3");
 ?>
 <div class="TableMenuBorder">
@@ -206,7 +206,7 @@ mysql_query($delboard);
 </tr></table></div>
 <?php } if($_GET['act']=="view"&&$_POST['update']!="now") {
 $query = query("SELECT * FROM `".$Settings['sqltable']."members` WHERE `id`=%i LIMIT 1", array($_SESSION['UserID']));
-$result=mysql_query($query);
+$result=exec_query($query);
 $num=mysql_num_rows($result);
 $i=0;
 $YourID=mysql_result($result,$i,"id");
@@ -260,7 +260,7 @@ if($ts_array[1]>59) { $ts_array[1] = "59"; $Settings['DefaultTimeZone'] = $ts_ar
 if($ts_array[1]<0) { $ts_array[1] = "00"; $Settings['DefaultTimeZone'] = $ts_array[0].":".$ts_array[1]; }
 $tsa = array("offset" => $Settings['DefaultTimeZone'], "hour" => $ts_array[0], "minute" => $ts_array[1]);
 $mguerys = query("SELECT * FROM `".$Settings['sqltable']."groups`", array(null));
-$mgresults=mysql_query($mguerys);
+$mgresults=exec_query($mguerys);
 $mnum=mysql_num_rows($mgresults);
 $mi = 0;
 while ($mi < $mnum) {

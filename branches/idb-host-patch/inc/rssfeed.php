632ae692ba4a9b2e4d60910c1fd3cef8b931f02c
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-11-29 23:19:26 +0000

    Small update to Host Mod Version.

--- branches/idb-host-patch/inc/rssfeed.php
+++ branches/idb-host-patch/inc/rssfeed.php
@@ -170,7 +170,7 @@ $RSS .= '<item rdf:about="'.$BoardURL.url_maker($exfilerss['topic'],$Settings['f
 if($_GET['feedtype']=="rss") {
 $CDataDescription = "<![CDATA[\n".$MyDescription."\n]]>";
 $RSS .= '<item>'."\n".'<pubDate>'.$TheTime.'</pubDate>'."\n".'<author>'.$UsersName.'</author>'."\n".'<title>'.$TopicName.'</title>'."\n".'<description>'.$CDataDescription.'</description>'."\n".'<link>'.$BoardURL.url_maker($exfilerss['topic'],$Settings['file_ext'],"act=view&id=".$TopicID."&page=1",$Settings['qstr'],$Settings['qsep'],$prexqstrrss['topic'],$exqstrrss['topic']).'</link>'."\n".'<guid>'.$BoardURL.url_maker($exfilerss['topic'],$Settings['file_ext'],"act=view&id=".$TopicID."&page=1",$Settings['qstr'],$Settings['qsep'],$prexqstrrss['topic'],$exqstrrss['topic']).'</guid>'."\n".'</item>'."\n"; } }
-++$i; } sql_free_result($result);
+++$i; sql_free_result($result); }
 sql_free_result($result);
 ++$glti; }
 xml_doc_start("1.0",$Settings['charset']);

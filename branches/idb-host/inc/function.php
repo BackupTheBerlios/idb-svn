b48cfed1fa0b30860b0d5db86832a46cfb9fbe94
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-12-03 03:15:55 +0000

    Added new smileys and changed smileys from gif to png.

--- branches/idb-host/inc/function.php
+++ branches/idb-host/inc/function.php
@@ -11,7 +11,7 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: function.php - Last Update: 12/02/2009 SVN 372 - Author: cooldude2k $
+    $FileInfo: function.php - Last Update: 12/02/2009 SVN 373 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="function.php"||$File3Name=="/function.php") {
@@ -215,7 +215,7 @@ if($_GET['debug']=="true"||
 $debug_on = true; } }
 $BoardURL = $Settings['idburl'];
 // Change URLs to Links
-function url2link($matches) {
+function pre_url2link($matches) {
 global $BoardURL; $opennew = true;
 $burlCHCK = parse_url($BoardURL);
 $urlCHCK = parse_url($matches[0]);

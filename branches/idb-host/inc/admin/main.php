86b811181c14e37ec6049f5cfd9c291b78554c30
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-12-09 06:59:06 +0000

    Changed mysql backticks to sql double quotes.

--- branches/idb-host/inc/admin/main.php
+++ branches/idb-host/inc/admin/main.php
@@ -11,7 +11,7 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: main.php - Last Update: 12/07/2009 SVN 380 - Author: cooldude2k $
+    $FileInfo: main.php - Last Update: 12/09/2009 SVN 382 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="main.php"||$File3Name=="/main.php") {
@@ -90,8 +90,8 @@ $Settings['KBoostPercent'] = "6|10"; }
 require($SettDir['admin'].'table.php'); 
 if($_GET['act']=="delsessions"&&$GroupInfo['ViewDBInfo']=="yes") {
 $time = GMTimeStamp() - ini_get("session.gc_maxlifetime");
-//$sqlg = sql_pre_query('DELETE FROM `'.$Settings['sqltable'].'sessions` WHERE `expires` < UNIX_TIMESTAMP();', array(null));
-$sqlgc = sql_pre_query('DELETE FROM `'.$Settings['sqltable'].'sessions` WHERE `expires` < %i', array($time));
+//$sqlg = sql_pre_query('DELETE FROM \"'.$Settings['sqltable'].'sessions\" WHERE \"expires\" < UNIX_TIMESTAMP();', array(null));
+$sqlgc = sql_pre_query("DELETE FROM \"".$Settings['sqltable']."sessions\" WHERE \"expires\" < %i", array($time));
 sql_query($sqlgc,$SQLStat);
 $_POST['update'] = "now"; $_GET['act'] = "view"; }
 if($_GET['act']=="optimize"&&$GroupInfo['ViewDBInfo']=="yes") {
@@ -106,7 +106,7 @@ $TblOptimized = 0;
 while ($ti < $tcount) {
 if(isset($OptimizeAr["Msg_text"])) { unset($OptimizeAr["Msg_text"]); }
 if(isset($OptimizeAr[3])) { unset($OptimizeAr[3]); }
-$OptimizeTea = sql_query(sql_pre_query("OPTIMIZE TABLE `".$TableChCk[$ti]."`", array(null)),$SQLStat); 
+$OptimizeTea = sql_query(sql_pre_query("OPTIMIZE TABLE \"".$TableChCk[$ti]."\"", array(null)),$SQLStat); 
 $OptimizeAr = sql_fetch_array($OptimizeTea);
 if(!isset($OptimizeAr["Msg_text"])&&
 	isset($OptimizeAr[3])) { $OptimizeAr["Msg_text"] = $OptimizeAr[3]; }
@@ -207,7 +207,7 @@ unset($_COOKIE[session_name()]);
 $_SESSION = array();
 session_unset();
 session_destroy();
-$delboard = 'DROP TABLE `'.$_GET['board'].'_categories`, `'.$_GET['board'].'_catpermissions`, `'.$_GET['board'].'_events`, `'.$_GET['board'].'_forums`, `'.$_GET['board'].'_groups`, `'.$_GET['board'].'_members`, `'.$_GET['board'].'_messenger`, `'.$_GET['board'].'_permissions`, `'.$_GET['board'].'_posts`, `'.$_GET['board'].'_restrictedwords`, `'.$_GET['board'].'_sessions`, `'.$_GET['board'].'_smileys`, `'.$_GET['board'].'_topics`, `'.$_GET['board'].'_wordfilter`';
+$delboard = 'DROP TABLE \"'.$_GET['board'].'_categories\", \"'.$_GET['board'].'_catpermissions\", \"'.$_GET['board'].'_events\", \"'.$_GET['board'].'_forums\", \"'.$_GET['board'].'_groups\", \"'.$_GET['board'].'_members\", \"'.$_GET['board'].'_messenger\", \"'.$_GET['board'].'_permissions\", \"'.$_GET['board'].'_posts\", \"'.$_GET['board'].'_restrictedwords\", \"'.$_GET['board'].'_sessions\", \"'.$_GET['board'].'_smileys\", \"'.$_GET['board'].'_topics\", \"'.$_GET['board'].'_wordfilter\"';
 sql_query($delboard,$SQLStat);
 redirect("refresh",$basedir.url_maker($exfile['index'],$Settings['file_ext'],"act=view",$Settings['qstr'],$Settings['qsep'],$prexqstr['index'],$exqstr['index'],FALSE),"3");
 ?>
@@ -235,7 +235,7 @@ redirect("refresh",$basedir.url_maker($exfile['index'],$Settings['file_ext'],"ac
 <td class="TableMenuColumn4">&nbsp;</td>
 </tr></table></div>
 <?php } if($_GET['act']=="view"&&$_POST['update']!="now") {
-$query = sql_pre_query("SELECT * FROM `".$Settings['sqltable']."members` WHERE `id`=%i LIMIT 1", array($_SESSION['UserID']));
+$query = sql_pre_query("SELECT * FROM \"".$Settings['sqltable']."members\" WHERE \"id\"=%i LIMIT 1", array($_SESSION['UserID']));
 $result=sql_query($query,$SQLStat);
 $num=sql_num_rows($result);
 $i=0;
@@ -289,7 +289,7 @@ if(!is_numeric($ts_array[1])) { $ts_array[1] = "00"; }
 if($ts_array[1]>59) { $ts_array[1] = "59"; $Settings['DefaultTimeZone'] = $ts_array[0].":".$ts_array[1]; }
 if($ts_array[1]<0) { $ts_array[1] = "00"; $Settings['DefaultTimeZone'] = $ts_array[0].":".$ts_array[1]; }
 $tsa = array("offset" => $Settings['DefaultTimeZone'], "hour" => $ts_array[0], "minute" => $ts_array[1]);
-$mguerys = sql_pre_query("SELECT * FROM `".$Settings['sqltable']."groups`", array(null));
+$mguerys = sql_pre_query("SELECT * FROM \"".$Settings['sqltable']."groups\"", array(null));
 $mgresults=sql_query($mguerys,$SQLStat);
 $mnum=sql_num_rows($mgresults);
 $mi = 0;

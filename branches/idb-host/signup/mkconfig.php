fb8bee2decc5f698dd19d00e90915e7f8b848ee5
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-12-11 23:14:02 +0000

    Added SQLite support.

--- branches/idb-host/signup/mkconfig.php
+++ branches/idb-host/signup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 12/10/2009 SVN 390 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 12/11/2009 SVN 396 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -179,6 +179,8 @@ if($Settings['sqltype']=="mysql") {
 require($SetupDir['setup'].'mysql.php'); }
 if($Settings['sqltype']=="pgsql") {
 require($SetupDir['setup'].'pgsql.php'); }
+if($Settings['sqltype']=="sqlite") {
+require($SetupDir['setup'].'sqlite.php'); }
 $CHMOD = $_SERVER['PHP_SELF'];
 $iDBRDate = $SVNDay[0]."/".$SVNDay[1]."/".$SVNDay[2];
 $iDBRSVN = $VER2[2]." ".$SubVerN;

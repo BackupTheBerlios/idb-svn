acaa670ecd914e2f8d579f4a59434b0aed0a17d7
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-12-12 07:38:00 +0000

    Added support for MySQL Improved Extension.

--- branches/idb-host-patch/signup/mkconfig.php
+++ branches/idb-host-patch/signup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2008 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 12/11/2009 SVN 396 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 12/12/2009 SVN 398 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -170,7 +170,8 @@ if($csrand==3) { $gpass .= chr(rand(97,122)); }
 ++$i; } $GuestPassword = b64e_hmac($gpass,$YourDate,$GSalt,"sha256");
 $url_this_dir = "http://".$_SERVER['HTTP_HOST']."/".$_POST['unixname']."/"."index.php?act=view";
 $YourIP = $_SERVER['REMOTE_ADDR'];
-if($Settings['sqltype']=="mysql") {
+if($Settings['sqltype']=="mysql"||
+	$Settings['sqltype']=="mysqli") {
 require($SetupDir['setup'].'mysql.php'); }
 if($Settings['sqltype']=="pgsql") {
 require($SetupDir['setup'].'pgsql.php'); }

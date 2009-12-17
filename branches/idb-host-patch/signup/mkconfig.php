695e27f222a3127578e3d6d910ad63e96b605384
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2009-12-17 21:19:09 +0000

    Small updates and changes. :P

--- branches/idb-host-patch/signup/mkconfig.php
+++ branches/idb-host-patch/signup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2008 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 12/12/2009 SVN 401 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 12/17/2009 SVN 418 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -144,7 +144,7 @@ $MyDay = GMTimeGet("d",$YourOffSet,0,$AdminDST);
 $MyMonth = GMTimeGet("m",$YourOffSet,0,$AdminDST);
 $MyYear = GMTimeGet("Y",$YourOffSet,0,$AdminDST);
 $MyYear10 = $MyYear+10;
-$YourDateEnd = $YourDate + $dayconv['month'];
+$YourDateEnd = $YourDate;
 $EventMonth = GMTimeChange("m",$YourDate,0,0,"off");
 $EventMonthEnd = GMTimeChange("m",$YourDateEnd,0,0,"off");
 $EventDay = GMTimeChange("d",$YourDate,0,0,"off");
--- branches/idb-host/signup/mkconfig.php
+++ branches/idb-host/signup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 12/12/2009 SVN 401 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 12/17/2009 SVN 418 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -149,7 +149,7 @@ $MyDay = GMTimeGet("d",$YourOffSet,0,$AdminDST);
 $MyMonth = GMTimeGet("m",$YourOffSet,0,$AdminDST);
 $MyYear = GMTimeGet("Y",$YourOffSet,0,$AdminDST);
 $MyYear10 = $MyYear+10;
-$YourDateEnd = $YourDate + $dayconv['month'];
+$YourDateEnd = $YourDate;
 $EventMonth = GMTimeChange("m",$YourDate,0,0,"off");
 $EventMonthEnd = GMTimeChange("m",$YourDateEnd,0,0,"off");
 $EventDay = GMTimeChange("d",$YourDate,0,0,"off");

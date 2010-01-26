3bc22bdc7d504a8a2a07c9cce02df6579671b6e1
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2010-01-26 19:25:04 +0000

    Updated Version Checker. It shows a green check mark when you have latest version. It shows a red check mark when you have a old version. It shows a yellow check mark when you have a new svn version.

--- branches/idb-host/setup/mkconfig.php
+++ branches/idb-host/setup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 01/22/2010 SVN 446 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 01/26/2010 SVN 451 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -261,7 +261,7 @@ $BoardSettings=$pretext2[0]."\n".
 "\$Settings['rss_ext'] = '.php';\n".
 "\$Settings['js_ext'] = '.js';\n".
 "\$Settings['showverinfo'] = 'on';\n".
-"\$Settings['vercheck'] = 2;\n".
+"\$Settings['vercheck'] = 1;\n".
 "\$Settings['fixpathinfo'] = 'off';\n".
 "\$Settings['fixbasedir'] = 'off';\n".
 "\$Settings['fixcookiedir'] = 'off';\n".

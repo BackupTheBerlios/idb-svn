a96a1edb36334608d30828b57789eb777ff5922d
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2010-05-10 06:05:53 +0000

    Small bug fix to time on setup/signup.

--- branches/idb-host/setup/mkconfig.php
+++ branches/idb-host/setup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 04/30/2010 SVN 472 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 05/10/2010 SVN 482 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -37,6 +37,8 @@ if($_POST['SQLThemes']!="on"&&$_POST['SQLThemes']!="off") {
 if($Settings['SeparateDatabase']!="no"&&
 	$Settings['SeparateDatabase']!="yes") {
 	$Settings['SeparateDatabase'] = "no"; }
+if(function_exists("date_default_timezone_set")) { 
+	@date_default_timezone_set("UTC"); }
 ?>
 <tr class="TableRow3" style="text-align: center;">
 <td class="TableColumn3" colspan="2">

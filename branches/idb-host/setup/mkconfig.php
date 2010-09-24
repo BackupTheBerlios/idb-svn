896aa52ca0134722bb9214ee5624ae05c03d5da4
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2010-09-24 23:54:52 +0000

    Added Theme Installer. :P

--- branches/idb-host/setup/mkconfig.php
+++ branches/idb-host/setup/mkconfig.php
@@ -12,7 +12,7 @@
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
     iDB Installer made by Game Maker 2k - http://idb.berlios.net/
 
-    $FileInfo: mkconfig.php - Last Update: 09/24/2010 SVN 561 - Author: cooldude2k $
+    $FileInfo: mkconfig.php - Last Update: 09/24/2010 SVN 562 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="mkconfig.php"||$File3Name=="/mkconfig.php") {
@@ -303,7 +303,7 @@ $BoardSettings=$pretext2[0]."\n".
 "\$Settings['send_pagesize'] = 'off';\n".$pretext2[2]."\n".
 "\$SettDir['maindir'] = '".$idbdir."';\n".
 "\$SettDir['inc'] = 'inc/';\n".
-"\$SettDir['inc'] = 'archive/';\n".
+"\$SettDir['archive'] = 'archive/';\n".
 "\$SettDir['misc'] = 'inc/misc/';\n".
 "\$SettDir['sql'] = 'inc/misc/sql/';\n".
 "\$SettDir['admin'] = 'inc/admin/';\n".

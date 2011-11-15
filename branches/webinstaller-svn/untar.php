aaef65d3a8ccec6b46cda78b9193061d8aa097fa
cooldude2k <cooldude2k@58fb6afa-320d-0410-b7f0-cc63377bad3a>
2011-11-15 05:25:47 +0000

    Small bug fix to iUnTAR. Thanks to Josep Sanz Campderros (http://saltos.net/). ^_^

--- branches/webinstaller-svn/untar.php
+++ branches/webinstaller-svn/untar.php
@@ -2,21 +2,23 @@
 /*
     This program is free software; you can redistribute it and/or modify
     it under the terms of the Revised BSD License.
-
+ 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     Revised BSD License for more details.
-
+ 
     Copyright 2004-2011 iDB Support - http://idb.berlios.de/
     Copyright 2004-2011 Game Maker 2k - http://gamemaker2k.org/
-	iUnTar ver. 4.6.2 by Kazuki Przyborowski - http://idb.berlios.net/
-
-    $FileInfo: untar.php - Last Update: 01/16/2011 Ver 4.6.2 - Author: cooldude2k $
+    iUnTar ver. 4.7 by Kazuki Przyborowski & Josep Sanz Campderros
+ 
+    $FileInfo: untar.php - Last Update: 11/14/2011 Ver 4.7 - Author: cooldude2k $
 */
 
-// PHP iUnTAR Version 4.6.2
+// PHP iUnTAR Version 4.7
 // license: Revised BSD license
+// Kazuki Przyborowski (http://ja.gamemaker2k.org/)
+// Josep Sanz Campderros (http://saltos.net/)
 function untar($tarfile,$outdir="./",$chmod=null,$extract=true,$lsonly=false,$findfile=null) {
 $TarSize = filesize($tarfile);
 $TarSizeEnd = $TarSize - 1024;
@@ -29,7 +31,8 @@ if($extract===true) {
 if($extract===true) {
 if($outdir!=""&&!file_exists($outdir)) {
 	mkdir($outdir,0777); } }
-$thandle = fopen($tarfile, "r");
+$thandle = fopen($tarfile, "rb");
+$i = 0;
 if($extract===false) {
 	$FileArray = null; $i = 0; }
 $outdir = preg_replace('{/$}', '', $outdir)."/";
@@ -38,8 +41,21 @@ $qfindfile = preg_quote($findfile,"/"); }
 if(!isset($findfile)) {
 $qfindfile = null; }
 while (ftell($thandle)<$TarSizeEnd) {
+	$FileName = null;
+	$FileMode = null;
+	$OwnerID = null;
+	$GroupID = null;
+	$FileSize = null;
+	$LastEdit = null;
+	$Checksum = null;
+	$FileType = null;
+	$LinkedFile = null;
+	$FileContent = null;
 	$FileName = $outdir.str_replace("trunk/","",trim(fread($thandle,100)));
-	if($findfile!==null&&!preg_match("/".$qfindfile."/",$FileName)) {
+	fseek($thandle,56,SEEK_CUR);
+	$FileType = trim(fread($thandle,1));
+	fseek($thandle,-57,SEEK_CUR);
+	if($findfile!==null&&$FileType!="L"&&!preg_match("/".$qfindfile."/",$FileName)) {
 		fseek($thandle,8,SEEK_CUR);
 		fseek($thandle,8,SEEK_CUR);
 		fseek($thandle,8,SEEK_CUR);
@@ -51,7 +67,7 @@ while (ftell($thandle)<$TarSizeEnd) {
 		fseek($thandle,255,SEEK_CUR); 
 		if($FileType=="0"||$FileType=="7") {
 			fseek($thandle,$FileSize,SEEK_CUR); } }
-	if($findfile===null||preg_match("/".$qfindfile."/",$FileName)) {
+	if($findfile===null||$FileType=="L"||preg_match("/".$qfindfile."/",$FileName)) {
 	$FileMode = trim(fread($thandle,8));
 	if($chmod===null) {
 		$FileCHMOD = octdec("0".substr($FileMode,-3)); }
@@ -64,13 +80,35 @@ while (ftell($thandle)<$TarSizeEnd) {
 		$Checksum = octdec(trim(fread($thandle,8)));
 		$FileType = trim(fread($thandle,1));
 		$LinkedFile = trim(fread($thandle,100));
-		fseek($thandle,255,SEEK_CUR); }
+		fseek($thandle,255,SEEK_CUR); 
+			// LongLink support added by Josep Sanz Campderros
+			if($FileType=="L"&&$FileSize>0) {
+				$FileName = $outdir.trim(fread($thandle,$FileSize));
+				fseek($thandle,512-$FileSize,SEEK_CUR);
+				fseek($thandle,100,SEEK_CUR);
+				$FileMode = trim(fread($thandle,8));
+				if($chmod===null) {
+					$FileCHMOD = octdec("0".substr($FileMode,-3)); }
+				if($chmod!==null) {
+					$FileCHMOD = $chmod; }
+				$OwnerID = trim(fread($thandle,8));
+				$GroupID = trim(fread($thandle,8));
+				$FileSize = octdec(trim(fread($thandle,12)));
+				$LastEdit = trim(fread($thandle,12));
+				$Checksum = trim(fread($thandle,8));
+				$FileType = trim(fread($thandle,1));
+				$LinkedFile = trim(fread($thandle,100));
+				fseek($thandle,255,SEEK_CUR); } }
 		if($findfile===null||preg_match("/".$qfindfile."/",$FileName)) {
 		if($FileType=="0"||$FileType=="7") {
 			if($lsonly===true) {
 			fseek($thandle,$FileSize,SEEK_CUR); }
 			if($lsonly===false) {
-			$FileContent = fread($thandle,$FileSize); } }
+			// Empty files support added by Josep Sanz Campderros
+			if($FileSize===0) {
+			$FileContent = ""; }
+			if($FileSize>0) {
+			$FileContent = fread($thandle,$FileSize); } } }
 		if($FileType=="1") {
 			$FileContent = null; }
 		if($FileType=="2") {
@@ -79,7 +117,7 @@ while (ftell($thandle)<$TarSizeEnd) {
 			$FileContent = null; }
 		if($FileType=="0"||$FileType=="7") {
 			if($extract===true) {
-				$subhandle = fopen($FileName, "a+");
+				$subhandle = fopen($FileName, "wb+");
 				fwrite($subhandle,$FileContent,$FileSize);
 				fclose($subhandle);
 				chmod($FileName,$FileCHMOD); } }
@@ -106,9 +144,12 @@ while (ftell($thandle)<$TarSizeEnd) {
 				if($lsonly===false) {
 				$FileArray[$i]['FileContent'] = $FileContent; } } } }
 		//touch($FileName,$LastEdit);
-		if($extract===false&&$findfile===null) { ++$i; }
-		if($findfile!==null&&preg_match("/".$qfindfile."/",$FileName)) { ++$i; }
-		if($FileType=="0"||$FileType=="7") {
+		if($extract===false&&$findfile===null&&isset($FileArray[$i]['FileName'])) { ++$i; }
+		if($extract===false) {
+		if($findfile!==null&&preg_match("/".$qfindfile."/",$FileName)&&isset($FileArray[$i]['FileName'])) { ++$i; } }
+		if($extract===true) {
+		if($findfile!==null&&preg_match("/".$qfindfile."/",$FileName)) { ++$i; } }
+		if(($FileType=="0"||$FileType=="7")&$FileSize>0) {
 			$CheckSize = 512;
 			while ($CheckSize<$FileSize) {
 				if($CheckSize<$FileSize) {
